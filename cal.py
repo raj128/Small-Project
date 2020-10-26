83fa364c6e45e68e74da3316ae1b031e158156b1
yashgautam9274 <72320108+yashgautam9274@users.noreply.github.com>
2020-10-27 00:31:15 +0530

    cal

---
+++ cal.py
@@ -0,0 +1,12 @@
+#calculator
+a=int(input("Enter the number="))
+b=int(input("Enter the number="))
+c=input("Enter the operators[ +,-,*,/]:")
+if c=='+':
+    print("Your Result",a+b)
+if c=='-':
+    print("Your Result",a-b)
+if c=='*':
+    print("Your Result",a*b)    
+if c=='/':
+    print("Your Result",a/b)
