ae0bc6ebb01afff1166922647713cbd191b2ad5a
yashgautam9274 <72320108+yashgautam9274@users.noreply.github.com>
2020-10-26 19:55:08 +0530

    update1

---
+++ guess_no.py
@@ -0,0 +1,13 @@
+print("Guess the number between 1 to 20")
+for x in range(1,4):
+    a=int(input("Enter the number="))
+    if a==6:
+        print("congrulation")
+        break
+    elif a>=7:
+        print("your number is large")
+    else:
+        print("your number is small")
+    
+
+
