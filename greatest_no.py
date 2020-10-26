4494007deba3ff8221093e39b3b87c6e1fdc570d
yashgautam9274 <72320108+yashgautam9274@users.noreply.github.com>
2020-10-27 00:06:32 +0530

    project2

---
+++ greatest_no.py
@@ -0,0 +1,15 @@
+#print the largest no
+a=int(input("Enter the number="))
+b=int(input("Enter the number="))
+c=int(input("Enter the number="))
+largest=a
+if b>largest :
+    largest=b
+else:
+    largest=a
+if c>largest:
+    largest=c
+print("largesrt no is=",largest)    
+    
+
+    
