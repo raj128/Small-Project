66898126db4c4ec419ab49b985ea2670bbaf1d83
yashgautam9274 <72320108+yashgautam9274@users.noreply.github.com>
2020-11-23 19:43:32 +0530

    update02

--- Pass_Generator.py
+++ Pass_Generator.py
@@ -1,25 +1,23 @@
 import string
 import random
 s1=string.ascii_lowercase
-#print(s1)
 s2=string.ascii_uppercase
-#print(s2)
 s3=string.digits
-#print(s3)
 s4=string.punctuation
-#print(s4)
-passlen=int(input("Enter the passward length:"))
 s=[]
 s.extend(list(s1))
 s.extend(list(s2))
 s.extend(list(s3))
 s.extend(list(s4))
 random.shuffle(s)
-try:
-    passlen=int(input("Enter the passward length:"))
-except:
-    print("Enter the integer like 4,5,7 etc ")
-    passlen=int(input("Enter the passward length:"))
+while True:
+    passlen=input("Enter the passward length:")
+    a=passlen.isdigit()
+    if a==True:
+         break
+    else:
+        print("Enter the number like 4,7,11 etc")
+passlen=int(passlen)    
 print("".join(s[0:passlen]))
     
     
