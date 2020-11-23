9128c1a58c6726d33a565a46e59c718ddc71e54d
yashgautam9274 <72320108+yashgautam9274@users.noreply.github.com>
2020-11-23 20:56:54 +0530

    update3

--- greatest_no.py
+++ greatest_no.py
@@ -1,12 +1,33 @@
-#print the largest no
-a=int(input("Enter the number="))
-b=int(input("Enter the number="))
-c=int(input("Enter the number="))
+print("Find out greatest no")
+while True:
+    a=input("Enter the number1=")
+    d=a.isdigit()
+    if d==True:
+        break
+    else:
+        print("Enter the integer like 3,6,1")
+a=int(a)
+while True:
+    b=input("Enter the number2=")
+    e=b.isdigit()
+    if e==True:
+        break
+    else:
+        print("Enter the integer like 3,6,1")
+b=int(b)
+while True:
+    c=input("Enter the number3=")
+    f=c.isdigit()
+    if f==True:
+        break
+    else:
+        print("Enter the integer like 3,6,1")
+c=int(c)        
 largest=a
 if b>largest :
     largest=b
 if c>largest:
     largest=c
-print("largesrt no is=",largest)    
+print("largest no is=",largest)    
 
     
